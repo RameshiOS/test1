dfcec017c9ed21daf14bfce0acf0307ac7aa7c14
manulogix <divya@manulogix.com>
2014-05-20 11:13:09 +0530

    Initial Commit

---
+++ RestaurantBash/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RestaurantBash
+//
+//  Created by Divya on 5/20/14.
+//  Copyright (c) 2014 Manulogix Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RestaurantBash/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RestaurantBash
+//
+//  Created by Divya on 5/20/14.
+//  Copyright (c) 2014 Manulogix Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
